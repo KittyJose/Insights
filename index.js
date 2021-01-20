361ac1da788c7d686ee481c14ab5f9ac7a20d0bd
Kitty Jose <54106287+KittyJose@users.noreply.github.com>
2021-01-20 15:12:52 +0000

    some changes

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 //const DBConnect = require("./src/connect");
 
 
-const core = require('./node_modules/@actions/core');
-const github = require('./node_modules/@actions/github');
+const core = require('./node_modules/@actions/core')
+const github = require('./node_modules/@actions/github')
+//const axios =  require('axios')
 
 try {
+    let axiosHub = axios.create()
 
-  const database = core.getInput('database');
-  console.log(`database ${database}!`);
+    let databaseUrl = core.getInput('database')
+    let key = core.getInput('key')
+    let json = core.getInput('json')
 
-  const key = core.getInput('key');
+    console.log(`database ${databaseUrl}!`)
+    console.log(`key ${key}!`)
+    console.log(`json ${json}!`)
+    console.log(`json ${json.job}!`)
 
-  console.log(`key ${key}!`);
+    /*const getHeader = async () => {
+        const options = {
+            mode: 'cors',
+            redirect: 'follow',
+            referrer: 'client',
+        }
+        options.headers = {Authorization: `Bearer ${key}`}
+        return options
+    }
 
-  const json = core.getInput('json');
+    const postCall = async () => {
+        const options = await getHeader()
+        const result = await axiosHub.post(`${databaseUrl}`, json, options)
+        return result
+    }
 
-  console.log(`json ${json}!`);
+    postCall()
 
-  core.setOutput("results", "some results");
+    const results = axios({
+        method: 'post',
+        url: '/kitty',
+        data: {json}
+    }) */
+
+    core.setOutput("results", results);
+
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
 
 
@@ -27,8 +52,8 @@ module.exports = (database, key, json) => {
     //let config = {server : "https://127.0.0.1:6363", key : "root", user: "admin", db:"Doc"}
     //DBConnect(config, json)
 
-    let inp= `database: ${database}, key: ${key}, json: ${json}`;
+    let inp= `database: ${databaseUrl}, key: ${key}, json: ${json}`;
     console.log(inp)
 
-    return `database: ${database}, key: ${key}, json: ${json}`;
+    return `database: ${databaseUrl}, key: ${key}, json: ${json}`;
 }
