cb47e56efcd7b45628183ed7123016c403de37b8
Kitty Jose <54106287+KittyJose@users.noreply.github.com>
2021-01-20 11:21:11 +0000

    cadsa

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const DBConnect = require("./src/connect");
+//const DBConnect = require("./src/connect");
 
 module.exports = (database, key, json) => {
     //let config = {server : "https://127.0.0.1:6363", key : "root", user: "admin", db:"Doc"}
